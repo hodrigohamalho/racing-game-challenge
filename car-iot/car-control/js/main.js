e6a0ab68831aa8911c8dca95000ffb210a47e72b
Rodrigo Ramalho <hodrigohamalho@gmail.com>
2020-03-29 19:05:48 -0300

    adjusts on the control

--- car-iot/car-control/js/main.js
+++ car-iot/car-control/js/main.js
@@ -9,108 +9,124 @@ $( document ).ready(function() {
     }
 
     function move(movementData){
-        console.log("Calling the websocket with the data: movementData");
+        console.log("Calling the websocket with the data:" +movementData);
+    }
+
+    function changeSpeed(speed){
+        console.log("Calling the speed websocket:" +speed);
     }
 
     function getSpeed(){
-        
+        return $("#speed").val();
     }
 
     var down = {}; // repeating calling issue
 
-    $(document).keydown(function(e) {
-        switch(e.which) {
-            case 37: // left
-            if (down['37'] == null) { // first press
-                pressButtonEffect("#left-button");
+    function enableKeyboardControl(){
+        // Case when the button is pressed
+        $(document).keydown(function(e) {
+            switch(e.which) {
+                case 37: // left
+                if (down['37'] == null) { // first press
+                    pressButtonEffect("#left-button");
+                    move({
+                        direction: "left",
+                        speed: getSpeed()
+                    });
+                    down['37'] = true;
+                }
+                break;
+
+                case 38: // up
+                if (down['38'] == null) { // first press
+                    pressButtonEffect("#up-button");
+                    move({
+                        direction: "up",
+                        speed: getSpeed()
+                    });
+                    down['38'] = true;
+                }
+                break;
+
+                case 39: // right
+                if (down['39'] == null) { // first press
+                    pressButtonEffect("#right-button");
+                    move({
+                        direction: "right",
+                        speed: getSpeed()
+                    });
+                    down['39'] = true;
+                }
+                break;
+
+                case 40: // down
+                if (down['40'] == null) { // first press
+                    pressButtonEffect("#down-button");
+                    move({
+                        direction: "down",
+                        speed: getSpeed()
+                    });
+                    down['40'] = true;
+                }
+                break;
+
+                default: return; // exit this handler for other keys
+            }
+            e.preventDefault(); // prevent the default action (scroll / move caret)
+        });
+
+
+        $(document).keyup(function(e) {
+            switch(e.which) {
+                case 37: // left
+                releaseButtonEffect("#left-button");
                 move({
-                    direction: "left",
+                    direction: "stop",
                     speed: getSpeed()
                 });
-                down['37'] = true;
-            }
-            break;
+                down['37'] = null;
+                break;
 
-            case 38: // up
-            if (down['38'] == null) { // first press
-                pressButtonEffect("#up-button");
+                case 38: // up
+                releaseButtonEffect("#up-button");
                 move({
-                    direction: "up",
+                    direction: "stop",
                     speed: getSpeed()
                 });
-                down['38'] = true;
-            }
-            break;
+                down['38'] = null;
+                break;
 
-            case 39: // right
-            if (down['39'] == null) { // first press
-                pressButtonEffect("#right-button");
+                case 39: // right
+                releaseButtonEffect("#right-button");
                 move({
-                    direction: "right",
+                    direction: "stop",
                     speed: getSpeed()
                 });
-                down['39'] = true;
-            }
-            break;
+                down['39'] = null;
+                break;
 
-            case 40: // down
-            if (down['40'] == null) { // first press
-                pressButtonEffect("#down-button");
+                case 40: // down
+                releaseButtonEffect("#down-button");
                 move({
-                    direction: "down",
+                    direction: "stop",
                     speed: getSpeed()
                 });
-                down['40'] = true;
-            }
-            break;
+                down['40'] = null;
+                break;
 
-            default: return; // exit this handler for other keys
-        }
-        e.preventDefault(); // prevent the default action (scroll / move caret)
+                default: return; // exit this handler for other keys
+            }
+            e.preventDefault(); // prevent the default action (scroll / move caret)
+        });
+    }
+
+    $("#speed").change(function(){
+        let newSpeed = $(this).val();
+        changeSpeed(newSpeed);
     });
 
+    // turn on the feature to control using the keyboard
+    enableKeyboardControl();
 
-    $(document).keyup(function(e) {
-        switch(e.which) {
-            case 37: // left
-            releaseButtonEffect("#left-button");
-            move({
-                direction: "stop",
-                speed: getSpeed()
-            });
-            down['37'] = null;
-            break;
-
-            case 38: // up
-            releaseButtonEffect("#up-button");
-            move({
-                direction: "stop",
-                speed: getSpeed()
-            });
-            down['38'] = null;
-            break;
-
-            case 39: // right
-            releaseButtonEffect("#right-button");
-            move({
-                direction: "stop",
-                speed: getSpeed()
-            });
-            down['39'] = null;
-            break;
-
-            case 40: // down
-            releaseButtonEffect("#bottom-button");
-            move({
-                direction: "stop",
-                speed: getSpeed()
-            });
-            down['40'] = null;
-            break;
-
-            default: return; // exit this handler for other keys
-        }
-        e.preventDefault(); // prevent the default action (scroll / move caret)
-    });
  
 });
